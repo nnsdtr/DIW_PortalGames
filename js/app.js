1006af8c2a9e1b6feaeaba7ee2dfbe214abbf574
Gabrielly <g4briellyluize@gmail.com>
2022-11-29 15:35:20 -0300

    Botão de carregar mais jogos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 const API_KEY = '1aa3bb39c3334036a3e8b13aefa463d3';
 
-// Exibe jogos - Abordagem FETCH
+let inicio = 0;
+let fim = 6;
+let pagina = 1;
 
-let xhr = new XMLHttpRequest()
-xhr.onload = function (event) {
-    let data = JSON.parse (event.target.response)
-    console.log (data.results)
+function exibeJogos () {
 
-    let str = ''
-    for(let i = 0; i < data.results.length; i++) {
-        let jogo = data.results[i]
-        str += `<div class="card col-md-5" style="width: 30rem;">
+    let sectionConteudoJogos = document.getElementById('conteudoJogos');
+    let str = sectionConteudoJogos.innerHTML;
+
+    // Montar texto HTML dos filmes
+    let dados = JSON.parse (this.responseText);
+
+    for (inicio; inicio < fim; inicio++) {
+        let jogo = dados.results[inicio];
+
+        str = str + `<div class="card col-md-5" style="width: 30rem;">
             <img src="${jogo.background_image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title"><b>${jogo.name}</b></h5>
@@ -19,12 +24,31 @@ xhr.onload = function (event) {
                 <a href="https://rawg.io/games/${jogo.id}" target="_blank" class="btn btn-dark">Mais detalhes</a>
             </div>
         </div>`
-    }
+        ;
+    };
 
-    document.getElementById('tela').innerHTML = str
+    switch (fim){
+        case 18:
+            fim++;
+            break;
+        case 19:
+            fim = 6;
+            inicio = 0;
+            pagina++;
+        default:
+            fim +=6;
+    }
+    sectionConteudoJogos.innerHTML = str;
 }
 
+function carregarMaisJogos () {
 
-xhr.open ('GET', 'https://api.rawg.io/api/games?key=1aa3bb39c3334036a3e8b13aefa463d3')
-xhr.send()
+    let xhr = new XMLHttpRequest ();
+    xhr.onload = exibeJogos;
+    xhr.open ('GET', `https://api.rawg.io/api/games?key=${API_KEY}&page=${pagina}`);
+    xhr.send ();
+}
+
+document.getElementById ('btnMaisJogos').addEventListener ('click', carregarMaisJogos);
+document.addEventListener('load' ,carregarMaisJogos());
 
--- js/appPesquisa.js
+++ js/appPesquisa.js
@@ -1,18 +1,28 @@
 const API_KEY = '1aa3bb39c3334036a3e8b13aefa463d3';
 
-// Exibe jogos - Abordagem FETCH
-// https://api.rawg.io/api/games?key=1aa3bb39c3334036a3e8b13aefa463d3
+let inicio = 0;
+let fim = 6;
+let pagina = 1;
 
-let query = document.getElementById('txtPesquisa').value;
-let xhr = new XMLHttpRequest()
-xhr.onload = function (event) {
-    let data = JSON.parse (event.target.response)
-    console.log (data.results)
+function filtro(){
+    var filtro = document.getElementById("textoFiltro").value;
+    exibeJogos(filtro.toLowerCase())
+}
 
-    let str = ''
-    for(let i = 0; i < data.results.length; i++) {
-        let jogo = data.results[i]
-        str += `<div class="card col-md-5" style="width: 30rem;">
+function exibeJogos (filtroBusca) {
+
+    let sectionConteudoJogos = document.getElementById('conteudoJogos');
+    let str = sectionConteudoJogos.innerHTML;
+
+    // Montar texto HTML dos filmes
+    let dados = JSON.parse (this.responseText);
+
+    for (inicio; inicio < fim; inicio++) {
+        let jogo = dados.results[inicio];
+
+        if(jogo.name.toLowerCase().startsWith(filtroBusca)){
+
+        str = str + `<div class="card col-md-5" style="width: 30rem;">
             <img src="${jogo.background_image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title"><b>${jogo.name}</b></h5>
@@ -20,38 +30,32 @@ xhr.onload = function (event) {
                 <p class="card-text">Avaliação: ${jogo.rating}</p>
                 <a href="https://rawg.io/games/${jogo.id}" target="_blank" class="btn btn-dark">Mais detalhes</a>
             </div>
-        </div>`
-    }
+        </div>`;
+        }
+    };
 
-    document.getElementById('tela').innerHTML = str
+    switch (fim){
+        case 18:
+            fim++;
+            break;
+        case 19:
+            fim = 6;
+            inicio = 0;
+            pagina++;
+        default:
+            fim +=6;
+    }
+    sectionConteudoJogos.innerHTML = str;
 }
 
-xhr.open ('GET', 'https://api.rawg.io/api/games?key=1aa3bb39c3334036a3e8b13aefa463d3')
-xhr.send()
+function carregarMaisJogos () {
 
-function exibePesquisa () {
-
-    let sectionConteudoPesquisa = document.getElementById('tela');
-    let str = sectionConteudoPesquisa.innerHTML;
-
-    for(let i = 0; i < data.results.length; i++) {
-        let jogo = data.results[i]
-        str += `<div class="card col-md-5" style="width: 30rem;">
-            <img src="${jogo.background_image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"><b>${jogo.name}</b></h5>
-                <p class="card-text">Lançamento: ${jogo.released}</p>
-                <p class="card-text">Avaliação: ${jogo.rating}</p>
-                <a href="https://rawg.io/games/${jogo.id}" target="_blank" class="btn btn-dark">Mais detalhes</a>
-            </div>
-        </div>`
-    }
-
-    document.getElementById('tela').innerHTML = str
-
+    let xhr = new XMLHttpRequest ();
+    xhr.onload = exibeJogos;
+    xhr.open ('GET', `https://api.rawg.io/api/games?key=${API_KEY}&page=${pagina}`);
+    xhr.send ();
 }
 
-xhr.open ('GET', `https://api.rawg.io/api/games?key=${API_KEY}&query=${query}`);
-xhr.send ();
+document.getElementById ('btnMaisJogos').addEventListener ('click', carregarMaisJogos);
+document.addEventListener('load' ,carregarMaisJogos());
 
-document.getElementById ('btnPesquisa').addEventListener ('click', exibePesquisa);
